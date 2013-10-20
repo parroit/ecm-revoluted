8b202cb4aabcb0e40daa2925e919888cbd3064b3
parroit <andrea.parodi@ebansoftware.net>
2013-10-20 12:45:02 +0200

    new errors and test

--- routes/_auth.js
+++ routes/_auth.js
@@ -16,36 +16,34 @@ exports.mount= function(app) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.post(
-        '/login',
-        function(req,res,next){
-            passport.authenticate('local',function(err, user, info){
-                if (err) { return next(err); }
-
-                if (user){
-                    req.logIn(user, function(err) {
-                        if (err) { return next(err); }
-                        res.redirect(req.query.redirect || '/');
-                    });
-
-                } else {
-                    res.redirect(
-                        '/login'+
-                            (req.query.redirect
-                                ? '?redirect='+req.query.redirect
-                                : '')
-                    );
-                }
-
-            })(req,res,next);
-        }
-    );
-
+    app.post('/login',loginRedirection);
     app.get('/login',login);
     app.get('/logout', logout);
 
     app.ensureAuthenticated=ensureAuthenticated;
 };
+
+function loginRedirection(req,res,next){
+    passport.authenticate('local',function(err, user, info){
+        if (err) { return next(err); }
+
+        if (user){
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                res.redirect(req.query.redirect || '/');
+            });
+
+        } else {
+            res.redirect(
+                '/login'+
+                    (req.query.redirect
+                        ? '?redirect='+req.query.redirect
+                        : '')
+            );
+        }
+
+    })(req,res,next);
+}
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
--- routes/aweforms.js
+++ routes/aweforms.js
@@ -1,40 +1,113 @@
 var cheerio = require('cheerio');
+var _ = require('lodash');
 
+exports.newInstance = function(i18n){
+    var aweforms = new Aweforms(i18n);
+    aweforms.field = _.bind(aweforms.field,aweforms);
+    return  aweforms;
+};
 
-exports.Aweforms = function(){
+var Aweforms = function(i18n){
 
+    if (i18n){
+        console.dir("custom i18n");
+        this.i18n = i18n;
+
+    } else {
+        console.dir("require i18n");
+        this.i18n = require("i18n");
+        this.i18n.configure({
+            locales: ['it'],
+            directory: __dirname + '/locales'
+        });
+
+    }
 };
 
 
 exports.init = function (req, res, next) {
     // mustache helper
-    res.locals.af = new exports.Aweforms();
+    res.locals.af =exports.newInstance();
 
     next();
 };
 
 
-exports.Aweforms.prototype.field = function(){
+function getFieldId($input, text) {
+    var qualifiedId = $input.attr("id");
+
+    if (!qualifiedId) {
+        throw new Error("id not defined in field " + text);
+    }
+
+    var idParts = qualifiedId.split(".");
+
+    var fieldName = idParts[1];
+    if (!fieldName) {
+        throw new Error("id should be fully qualified in field " + text);
+    }
+
+    var modelName = idParts[0];
+    if (!modelName) {
+        throw new Error("id should be fully qualified in field " + text);
+    }
+
+    return {
+        modelName: modelName,
+        name: fieldName,
+        qualifiedId:qualifiedId
+    };
+}
+
+function getError(fieldError) {
+    var error;
+    if (fieldError) {
+        if (fieldError.indexOf("!") == 0) {
+            error = eval('[' + fieldError.substring(1) + ']');
+
+        } else {
+            error = [fieldError];
+        }
+    } else {
+        error = [""];
+    }
+    return error;
+}
+Aweforms.prototype.field = function(){
     var self = this;
+
     return function(text, render) {
+
         var $ = cheerio.load(text);
         var $input = $("input");
 
-        var qualifiedId =  $input.attr("id");
-        var idParts = qualifiedId.split(".");
-        $input
-            .attr("name", idParts[1])
-            .attr("value", "{{" + qualifiedId + "}}");
+        var field = getFieldId($input, text);
 
-        var errors = this[idParts[0]].errors;
-        var fieldError = errors && errors[idParts[1]]  && errors[idParts[1]].type;
+        var modelValue = this[field.modelName];
+        var fieldValue = modelValue[field.name];
 
+        $input.attr("name", field.name);
+
+        if ($input.attr("type").toLowerCase() == "checkbox") {
+
+            if (fieldValue) {
+                $input.attr("checked", "checked")
+            }
+        } else {
+            $input.attr("value",fieldValue);
+        }
+
+        var errors = modelValue.errors;
+        var fieldError = errors && errors[field.name] && errors[field.name].type;
+
+        var error = getError(fieldError);
+        //console.log("field.qualifiedId: "+field.qualifiedId);
         return [
-            '<div class="field ',(fieldError ? "text-error":""),'">',
-            '<label for="', qualifiedId, '">{{#t}}', qualifiedId,'{{/t}}</label>',
+            '<div class="field ', (fieldError ? "text-error" : ""), '">',
+            '<label for="', field.qualifiedId, '">', self.i18n.__.call(this,field.qualifiedId), '</label>',
             '<div>',
             $.html(),
-            '<span class="error">{{#t}}',fieldError,'{{/t}}</span>',
+            '<span class="error">', self.i18n.__.apply(this, error), '</span>',
             '</div>',
             '</div>'
         ].join("");
